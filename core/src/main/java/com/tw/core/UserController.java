8e6bf97ee8e9efe0da90e4bc25f06ad296ea3507
ximwang <wangximingde@163.com>
2015-07-15 09:07:04 +0800

    add session

--- core/src/main/java/com/tw/core/UserController.java
+++ core/src/main/java/com/tw/core/UserController.java
@@ -4,17 +4,20 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/")
+@SessionAttributes("user")
 public class UserController {
     @Autowired
     private UserService userService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView getIndex() {
+    public ModelAndView getIndex(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("test");
-
         modelAndView.addObject("users", userService.getUsers());
         return modelAndView;
     }
@@ -53,15 +56,17 @@ public class UserController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public ModelAndView getLoginPage () {
+    public ModelAndView getLoginPage (HttpServletRequest request, HttpServletResponse response) {
+        request.getSession().setAttribute("u", "u");
         return new ModelAndView("login");
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public ModelAndView login (@RequestParam String name, String password) {
+    public ModelAndView login (@RequestParam String name, String password, HttpServletRequest request, HttpServletResponse response) {
 
         User user = userService.login(name);
         if(MD5Util.md5(password).equals(user.getPassword())) {
+            request.getSession().setAttribute("user", user);
             return new ModelAndView("redirect:/");
         }
         return new ModelAndView("redirect:/login");
